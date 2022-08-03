80d43ff9018d957504960fe7331ebda59691d9b8
lycol <505013562@qq.com>
2022-08-04 00:55:41 +0800

    #Add retry method only when happens with throw the target exceptions using spring aop method interceptor

--- src/main/java/com/jielu/config/MethodRetryInterceptorConfig.java
+++ src/main/java/com/jielu/config/MethodRetryInterceptorConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class InterceptorConfig {
+public class MethodRetryInterceptorConfig {
     public static final String traceExecution = "execution (* com.jielu.service..*.*(..)) && " +
             "@annotation(com.jielu.config.RetryFlag)";
 
